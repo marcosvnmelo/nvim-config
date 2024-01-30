575be1f20b4733ed5af760c9769e4136f61b9a56
Marcos Melo <suiciniv2016+github@outlook.com>
2024-01-30 10:08:51 -0300

    initial commit

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,14 @@
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      lua = { "stylua" },
+      javascript = { "prettierd" },
+      typescript = { "prettierd" },
+      typescriptreact = { "prettierd" },
+      html = { "prettierd" },
+      css = { "prettierd" },
+      json = { "prettierd" },
+    },
+  },
+}
---
+++ lua/plugins/mason.lua
@@ -0,0 +1,13 @@
+return {
+  "williamboman/mason.nvim",
+  opts = {
+    ensure_installed = {
+      "stylua",
+      "shellcheck",
+      "shfmt",
+      "eslint-lsp",
+      "prettierd",
+      "quick-lint-js",
+    },
+  },
+}
