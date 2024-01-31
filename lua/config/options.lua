1ff180d99acbe9fcbf65a923e012150a08eaa00e
Marcos Melo <suiciniv2016+github@outlook.com>
2024-01-30 23:07:13 -0300

    feat: setup spell

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,4 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.opt.winbar = "%=%m %f"
+-- vim.opt.winbar = "%=%m %f"
+vim.opt.spelllang = { "en_us", "pt_br" }
+vim.opt.spell = true
