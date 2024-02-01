bb96c6e44cd63816d0952d332d1435015e94d275
Marcos Melo <suiciniv2016+github@outlook.com>
2024-01-31 23:05:36 -0300

    fix: angularls enabled on non angular dir

--- lua/custom_lsp/angularls.lua
+++ lua/custom_lsp/angularls.lua
@@ -1,5 +1,9 @@
 local util = require("lspconfig.util")
 
 require("lspconfig").angularls.setup({
-  root_dir = util.root_pattern("angular.json", "project.json"),
+  root_dir = util.root_pattern(
+    "angular.json",
+    -- "project.json",
+    "karma.conf.js"
+  ),
 })
