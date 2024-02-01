69aab492016c1eb5a233a6971cf65677595ef4c5
Marcos Melo <suiciniv2016+github@outlook.com>
2024-01-31 23:29:32 -0300

    feat: setup quick-lint-js

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 require("custom_lsp.angularls")
+require("custom_lsp.quick_lint_js")
---
+++ lua/custom_lsp/quick_lint_js.lua
@@ -0,0 +1,3 @@
+require("lspconfig").quick_lint_js.setup({
+  filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
+})
