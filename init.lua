552153db561613565e835cbf20ff3eec246919d2
Marcos Melo <suiciniv2016+github@outlook.com>
2024-02-11 14:16:33 -0300

    feat: prisma lsp setup

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 -- require("custom.lsp.angularls")
+require("custom.lsp.prismals")
 -- require("custom.lsp.quick_lint_js")
 require("custom.plugins.notify")
