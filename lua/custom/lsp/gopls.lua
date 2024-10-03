d48c549fbd54164bd863e4dbd24f58b0af9b278a
Marcos Melo <suiciniv2016+github@outlook.com>
2024-10-03 19:40:56 -0300

    refactor(gopls): move gopls config to lspconfig file

--- lua/custom/lsp/gopls.lua
+++
@@ -1,5 +0,0 @@
-local util = require("lspconfig.util")
-
-require("lspconfig").gopls.setup({
-  root_dir = util.root_pattern("go.work", "go.mod"),
-})
--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,8 +1,15 @@
+local util = require("lspconfig.util")
+
 return {
   "neovim/nvim-lspconfig",
   opts = {
     inlay_hints = {
       enabled = false,
+    },
+    setup = {
+      gopls = function(_, opts)
+        opts.root_dir = util.root_pattern("go.work", "go.mod")
+      end,
     },
   },
 }
