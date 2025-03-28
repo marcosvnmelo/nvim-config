3cfca3479c7c05799e0e7f36ced6c0d4dc570ba3
Marcos Melo <suiciniv2016+github@outlook.com>
2025-03-28 15:45:47 -0300

    feat: add autocmd for fish file type with LSP integration

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -8,3 +8,14 @@ vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
   pattern = { "*.hl", "hypr*.conf" },
   command = "setfiletype hyprlang",
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "fish",
+  callback = function()
+    vim.lsp.start({
+      name = "fish-lsp",
+      cmd = { "fish-lsp", "start" },
+      cmd_env = { fish_lsp_show_client_popups = false },
+    })
+  end,
+})
