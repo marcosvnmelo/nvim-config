d1f42f1d3318188b82d558e95f23b177dbf1f800
Marcos Melo <suiciniv2016+github@outlook.com>
2025-05-27 10:39:28 -0300

    refactor: remove items from lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,12 +3,25 @@ return {
   event = "VeryLazy",
   icon = nil,
   opts = function(_, opts)
+    local icons = LazyVim.config.icons
+
     opts.sections.lualine_a = {
       {
         "mode",
         icon = "",
         -- icon = "",
         color = { gui = "bold" },
+      },
+    }
+    opts.sections.lualine_c = {
+      {
+        "diagnostics",
+        symbols = {
+          error = icons.diagnostics.Error,
+          warn = icons.diagnostics.Warn,
+          info = icons.diagnostics.Info,
+          hint = icons.diagnostics.Hint,
+        },
       },
     }
   end,
